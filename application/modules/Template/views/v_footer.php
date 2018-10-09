5760f50bf458a2143ab6ee2113f5173cc2a929cb
qodrisZiad <qodrisZiad@users.noreply.github.com>
2018-10-09 15:42:47 +0700

    master properti
    update master properti

--- application/modules/Template/views/v_footer.php
+++ application/modules/Template/views/v_footer.php
@@ -7,8 +7,7 @@
           <div class="clearfix"></div>
         </footer>
 		</div>
-	</div>
-    <script src="<?php echo site_url()?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+	</div> 
     <script src="<?php echo site_url()?>vendors/fastclick/lib/fastclick.js"></script>
     <script src="<?php echo site_url()?>vendors/nprogress/nprogress.js"></script>
     <script src="<?php echo site_url()?>build/js/custom.min.js"></script>
