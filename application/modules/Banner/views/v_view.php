4aeae42f63093566f04ebf5063f35e64b35b83ee
qodrisZiad <qodrisZiad@users.noreply.github.com>
2018-10-19 22:04:19 +0700

    update header banner
    header salah

--- application/modules/Banner/views/v_view.php
+++ application/modules/Banner/views/v_view.php
@@ -3,7 +3,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Master Kategori</h2>
+                    <h2>Master <?= $bread; ?></h2>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li> 
                       <li><a><i class=""></i></a></li>
